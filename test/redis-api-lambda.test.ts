e9f3751600a1dac71209074cb6c5ef4da2cdb76a
FazlOmar9 <bt22csa026@iiitn.ac.in>
2025-08-08 23:40:28 +0530

    Initial commit

---
+++ test/redis-api-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RedisApiLambda from '../lib/redis-api-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/redis-api-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RedisApiLambda.RedisApiLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
